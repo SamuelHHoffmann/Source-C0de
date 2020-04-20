15de4999bbfd294896cdd83028ff3d30f2ced6c0
samuelhhoffmann <samuel.hoffmann@stonybrook.edu>
2020-04-20 17:56:31 -0400

    added a temp load scene. not sure if we want to use glup to make things better

--- public/benchmark2/game.js
+++ public/benchmark2/game.js
@@ -1,52 +1,14 @@
+
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
-    }
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: LoadingScene
 };
 
 var game = new Phaser.Game(config);
-
-function preload() {
-    this.load.image('raster', 'assets/demoscene/raster-bw-800x16.png');
-}
-
-function create() {
-    var group = this.add.group();
-
-    group.createMultiple({ key: 'raster', repeat: 64 });
-
-    var hsv = Phaser.Display.Color.HSVColorWheel();
-
-    var i = 0;
-
-    var _this = this;
-
-    group.children.iterate(function (child) {
-
-        child.x = 400;
-        child.y = 100;
-        child.depth = 64 - i;
-
-        child.setTint(hsv[i * 4].color);
-
-        i++;
-
-        _this.tweens.add({
-            targets: child,
-            props: {
-                y: { value: 500, duration: 1500 },
-                scaleX: { value: child.depth / 64, duration: 6000, hold: 2000, delay: 2000 }
-            },
-            yoyo: true,
-            repeat: -1,
-            ease: 'Sine.easeInOut',
-            delay: 32 * i
-        });
-
-    });
-}
