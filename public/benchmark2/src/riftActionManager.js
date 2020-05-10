7dc4ac6c723b70e3d57cf618994119de4dfdfb92
samuelhhoffmann <samuel.hoffmann@stonybrook.edu>
2020-05-10 10:31:22 -0400

    RiftActionManager functions built out, need to put them into the implementation

--- public/benchmark2/src/riftActionManager.js
+++ public/benchmark2/src/riftActionManager.js
@@ -10,12 +10,40 @@ class RiftActionManager {
 
     static restoreIDStack = [];
 
-    static reverseToLevel(levelNumber) {
+    static quitIDStack = []
 
+    static reverseToLevel(levelNumber) {
+        for (var x = 0; x < this.idStack.length(); x++) {
+            var id = "" + this.idStack.pop();
+            if (id.charAt(0) > levelNumber) {
+                this.restoreIDStack.push(id);
+            } else if (id.charAt(0) == levelNumber) {
+                this.quitIDStack.push(id);
+                var reverseFunction = this.getInverseFunctionForID(id);
+                reverseFunction;
+            } else {
+                this.idStack.push(id);
+                break;
+                // ignore if actions are before this level. (like if you did level 1,2 ,3 4 and then went back and did level 2 you should leave whatever you did for level 1)
+            }
+        }
     }
 
-    static restoreStack(levelNumber) {
-
+    static restoreStack(didQuit) {
+        if (didQuit) {
+            for (var x = 0; x < this.quitIDStack.length(); x++) {
+                var quitID = this.quitIDStack.pop();
+                var quitFnAction = this.getFunctionForID(quitID);
+                quitFnAction();
+                this.idStack.push(quitID);
+            }
+        }
+        for (var x = 0; x < this.restoreIDStack.length(); x++) {
+            var restoreID = this.restoreIDStack.pop();
+            restoreFunction = this.getFunctionForID(restoreID);
+            restoreFunction();
+            this.idStack.push(restoreID);
+        }
     }
 
     static getFunctionForID(riftID, inputID) {
